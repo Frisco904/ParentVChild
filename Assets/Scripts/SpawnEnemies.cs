3fc4e2e44d66080cb2ccf671a19967b3c2517869
Giodude94 <Giovanni_vega94@yahoo.com>
2024-10-03 12:25:27 -0500

    Prefabs and Layer changes
    Added the rest of the prefabs and scripts. Added two layers one for the Projectile and Enemy and changed the collision for projectile to hit only enemy layered objects.

---
+++ Assets/Scripts/SpawnEnemies.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnEnemies : MonoBehaviour
+{
+    public GameObject enemyType;
+    public float spawnFrequency;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnEnemy", 0.0f, spawnFrequency);
+    }
+
+    void SpawnEnemy()
+    {
+        Instantiate(enemyType, transform);
+    }
+
+}
